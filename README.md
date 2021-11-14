85cd56b4b21e1408f7ffa77237919445d898477c
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-14 13:05:27 +0700

    fix(logic): deobfuscate by char sequence

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # huldo
+
+```sh
+./huldo.py '4n0nym0u5'
+anonymous
+```
+
+```sh
+./huldo.py -r 'anonymous'
+4n0nym0u5
+```
+
+```sh
+./huldo.py -rR -d ru 'Шла Саша по шоссе'
+W/|a СAw@ пo wocc3
+```
+
+```sh
+./huldo.py -d ru 'W/|a СAw@ пo wocc3'
+Шла Саша по шоссе
+```
--- huldo.py
+++ huldo.py
@@ -5,20 +5,18 @@ from argparse import ArgumentParser
 from random import shuffle
 
 DIR = Path(__file__).resolve().parent
-RU = DIR / 'ru.txt'
 
 
-def main(text, rev=False, random=False):
-    sp = map(str.split, RU.read_text().splitlines(False))
+def deobfuscate(d, text):
+    for k,v in d:
+        text = text.replace(k, v)
+    return text
 
-    if rev:
-        d = [(v, k) for k, v in sp]
-    else:
-        d = [(k, v) for k, v in sp]
 
+def obfuscate(d, text, random):
     res = []
     for c in text:
-        if rev and random:
+        if random:
             shuffle(d)
 
         found = False
@@ -29,13 +27,28 @@ def main(text, rev=False, random=False):
                 break
         if not found:
             res.append(c)
-    print(''.join(res))
+
+    return ''.join(res)
+
+
+def main(text, rev=False, random=False, dictionary='en'):
+    RU = DIR / ('%s.txt' % dictionary)
+    sp = map(str.split, RU.read_text().splitlines(False))
+
+    if rev:
+        d = [(v, k) for k, v in sp]
+        print(obfuscate(d, text, random))
+    else:
+        d = [(k, v) for k, v in sp]
+        print(deobfuscate(d, text))
+
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument('-r', '--reverse', default=False, action='store_true')
     p.add_argument('-R', '--random', default=False, action='store_true')
+    p.add_argument('-d', '--dictionary', type=str, default='en')
     p.add_argument('text', type=str)
     a = p.parse_args()
-    main(a.text, a.reverse, a.random)
+    main(a.text, a.reverse, a.random, a.dictionary)
